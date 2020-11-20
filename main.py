86372979719bb107f4b142ca04e108755ec8919f
Kirill362 <64096313+Kirill362@users.noreply.github.com>
2020-11-20 19:48:35 +0300

    Исправил недочёты

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
 from terminaltables import AsciiTable
 
+LANGUAGES = ["JavaScript", "Java", "Python", "Ruby", "PHP", "C++", "CSS", "C#"]
+HEADERS = {"X-Api-App-Id": "v3.h.3969469.05b92e1fe4e00b35711d249709174f06dc4bde24.fd893b9db5354288d7028c8be578ecd46699f697"}
+
 
 def creating_table(info, title):
     table_info = [["languages", "vacancies_found", "vacancies_processed", "average_salary"]]
@@ -30,62 +33,63 @@ def predict_rub_salary_sj(vacancie):
         return predict_salary(vacancie["payment_from"], vacancie["payment_to"])
 
 
-hh_answer = {}
-sj_answer = {}
-languages = ["JavaScript", "Java", "Python", "Ruby", "PHP", "C++", "CSS", "C#"]
-headers = {"X-Api-App-Id": "v3.h.3969469.05b92e1fe4e00b35711d249709174f06dc4bde24.fd893b9db5354288d7028c8be578ecd46699f697"}
-for lang in languages:
-    average_salary = 0
-    vacancies_processed = 0
-    payload = {"text": f"Программист {lang}",
-               "area": 1,
-               "per_page": 50}
-    hh_url = "https://api.hh.ru/vacancies"
-    response = requests.get(hh_url, params=payload)
-    response.raise_for_status()
-    for page in range(response.json()["pages"]):
+def main():
+    hh_answer = {}
+    sj_answer = {}
+    for lang in LANGUAGES:
+        average_salary = 0
+        hh_vacancies_processed = 0
         payload = {"text": f"Программист {lang}",
                    "area": 1,
-                   "per_page": 50,
-                   "page": page}
+                   "per_page": 50}
+        hh_url = "https://api.hh.ru/vacancies"
         response = requests.get(hh_url, params=payload)
         response.raise_for_status()
-        for vacancie in response.json()["items"]:
-            salary = predict_rub_salary_hh(vacancie)
-            if salary:
-                average_salary += salary
-                vacancies_processed += 1
-    hh_answer[lang] = {"vacancies_found": response.json()["found"],
-                   "vacancies_processed": vacancies_processed,
-                   "average_salary": int(average_salary / vacancies_processed)}
-print(creating_table(hh_answer, "HeadHunter Moscow"))
-
-for lang in languages:
-    sum_salary = 0
-    vacancies_processed = 0
-    payload = {"town": 4,
-               "catalogues": 48,
-               "keyword": lang}
-    hh_url = "https://api.superjob.ru/2.0/vacancies/"
-    response = requests.get(hh_url, params=payload, headers=headers)
-    response.raise_for_status()
-    for page in range(response.json()["total"] // 20 + 1):
+        for page in range(response.json()["pages"]):
+            payload = {"text": f"Программист {lang}",
+                       "area": 1,
+                       "per_page": 50,
+                       "page": page}
+            response = requests.get(hh_url, params=payload)
+            response.raise_for_status()
+            for vacancie in response.json()["items"]:
+                salary = predict_rub_salary_hh(vacancie)
+                if salary:
+                    average_salary += salary
+                    hh_vacancies_processed += 1
+        hh_answer[lang] = {"vacancies_found": response.json()["found"],
+                       "vacancies_processed": hh_vacancies_processed,
+                       "average_salary": int(average_salary / hh_vacancies_processed)}
+        sum_salary = 0
+        sj_vacancies_processed = 0
         payload = {"town": 4,
                    "catalogues": 48,
-                   "keyword": lang,
-                   "page": page}
-        response = requests.get(hh_url, params=payload, headers=headers)
+                   "keyword": lang}
+        hh_url = "https://api.superjob.ru/2.0/vacancies/"
+        response = requests.get(hh_url, params=payload, headers=HEADERS)
         response.raise_for_status()
-        for vacancie in response.json()["objects"]:
-            salary = predict_rub_salary_sj(vacancie)
-            if salary:
-                sum_salary += salary
-                vacancies_processed += 1
-    if vacancies_processed != 0:
-        average_salary = int(sum_salary / vacancies_processed)
-    else:
-        average_salary = None
-    sj_answer[lang] = {"vacancies_found": response.json()["total"],
-                   "vacancies_processed": vacancies_processed,
-                   "average_salary": average_salary}
-print(creating_table(sj_answer, "SuperJob Moscow"))
+        for page in range(response.json()["total"] // 20 + 1):
+            payload = {"town": 4,
+                       "catalogues": 48,
+                       "keyword": lang,
+                       "page": page}
+            response = requests.get(hh_url, params=payload, headers=HEADERS)
+            response.raise_for_status()
+            for vacancie in response.json()["objects"]:
+                salary = predict_rub_salary_sj(vacancie)
+                if salary:
+                    sum_salary += salary
+                    sj_vacancies_processed += 1
+        if sj_vacancies_processed != 0:
+            average_salary = int(sum_salary / sj_vacancies_processed)
+        else:
+            average_salary = None
+        sj_answer[lang] = {"vacancies_found": response.json()["total"],
+                       "vacancies_processed": sj_vacancies_processed,
+                       "average_salary": average_salary}
+    print(creating_table(hh_answer, "HeadHunter Moscow"))
+    print(creating_table(sj_answer, "SuperJob Moscow"))
+
+
+if __name__ == '__main__':
+  main()
